41cf8694f59a400e8625f0451379df3b97ee6d53
Dariana Green <darianapro@gmail.com>
2019-10-15 14:39:59 +0200

    JS added

---
+++ HTML Web/js/script.js
@@ -0,0 +1,11 @@
+console.log ("Hola Mundo");
+console.log ("hola de nuevo");
+var peso  = prompt("Cual es su masa del cuerpo en kg?");
+var pesoInt = parseInt(peso);
+var altura = prompt("Cual es su altura en cm?");
+var alturaInt = parseint(altura);
+
+var respuestaInt = pesoInt(alturaInt*alturaInt)/100;
+
+var respuesta = ("Su IMC = " + pesoInt);
+
